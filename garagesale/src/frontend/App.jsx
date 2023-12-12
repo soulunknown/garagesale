755547e188d8795254442c456ead767bbc1efc1c
soulunknown <131215509+soulunknown@users.noreply.github.com>
2023-12-12 15:18:11 -0600

    app.jsx

--- garagesale/src/frontend/App.jsx
+++ garagesale/src/frontend/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import WelcomeBanner from "./Welcomebanner";
-import Navbar from "./Navbar"
+import Navbar from "./Navbar";
+import Signup from "./signup";
+
 
 function App() {
   return (
     <div className="App">
       <WelcomeBanner />
       < Navbar />
+      <Signup />
     </div>
   );
 }
--- garagesale/src/frontend/main.jsx
+++ garagesale/src/frontend/main.jsx
@@ -1,9 +1,9 @@
-// index.js or main.jsx
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { ThemeProvider } from '@mui/material/styles';
-import theme from './theme'; // Ensure the path is correct
+import theme from './theme'; 
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
